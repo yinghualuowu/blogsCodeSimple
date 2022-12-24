bc850e08b56f6c1490bde5ae3a9c639bc0aba8ad
yinghualuowu <1048146365@qq.com>
2022-12-25 02:12:55 +0800

    虚方法的调用关系

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ https://www.cnblogs.com/yinghualuowu/p/16991373.html C# 反射以及实际场景
 
 ## ninjamustdie3Kmp
 https://www.cnblogs.com/yinghualuowu/p/13128549.html 忍者必须死3 模拟器按键设置
+
+## VirtualClass
+虚方法的调用关系，暂无链接
---
+++ VirtualClass/VirtualClass/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography.X509Certificates;
+
+D d = new D();
+C c = d;
+B b = d;
+A a = d;
+
+d.Foo();
+c.Foo();
+b.Foo();
+a.Foo();
+
+//D
+//C
+//C
+//A
+/*
+   D自然输出d，因为以下没有派生
+   C输出c，因为D存在new，所以隐藏，那么对于C来说，是B最远的派生
+   B输出c，因为C是B最远的派生
+   A没有任何基类
+ */
+Console.Read();
+
+class A
+{
+    public void Foo()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+class B : A
+{
+    public virtual void Foo()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class C : B
+{
+    public override void Foo()
+    {
+        Console.WriteLine("C");
+    }
+}
+
+class D : C
+{
+    public new void Foo()
+    {
+        Console.WriteLine("D");
+    }
+}
+
+
