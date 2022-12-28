f7e85e91cde5ba5fdd3cf2bcd640e775fab95d39
yinghualuowu <1048146365@qq.com>
2022-12-28 23:40:45 +0800

    委托和闭包

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+using Delegate;
+
+Test.Foo();
+
+// Foo foo = Sum;
+// foo += Minus;
+// Foo re = foo;
+// re -= Minus;
+//
+// foreach (var del in foo.GetInvocationList())
+// {
+//     Console.WriteLine(del.Method);
+// }
+//
+// //Func<int, int, int> func = Sum;
+//
+// //Action ac = OutPut;
+//
+// //func.Invoke(1, 2);
+//
+// static int Sum(int a, int b) => a + b;
+//
+// static int Minus(int a, int b) => a - b;
+//
+// static void OutPut()
+// {
+//     Console.WriteLine("action");
+// }
+//
+// internal delegate int Foo(int a, int b);
---
+++ Delegate/Delegate/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public static class Test
+    {
+        public static void Foo()
+        {
+            List<Action> ac = new List<Action>();
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    var i1 = i;
+            //    ac.Add(() => { Console.WriteLine(i1);});
+            //}
+
+            List<int> li = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (var value in li)
+            {
+                ac.Add(() => { Console.WriteLine(value); });
+            }
+
+            foreach (var action in ac)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
